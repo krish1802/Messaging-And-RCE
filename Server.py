de533d5e958e13dea81b4327755de2f0ad769b2c
krish1802 <69857495+krish1802@users.noreply.github.com>
2021-01-01 21:38:54 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,12 +1,13 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#For UDP Protocols
+while True:
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#For UDP Protocols
 
-ip = "0.0.0.0" #This will Listen to all clients trying for Connections
-port = 5555
+  ip = "0.0.0.0" #This will Listen to all clients trying for Connections
+  port = 5555
 
-s.bind((ip, port)) #Binds with the ip on port 5555
-x = s.recvfrom(1024) #receives message from the client
-clientip = x[1][0] #Shows ClientIP
-encodedMessage = x[0] #Shows Message In Bytes[DataType]
-message = encodedMessage.decode() #Decodes Byte DataType to Str
-print(clientip + ": " + message)#Displays the message with ClientIP
+  s.bind((ip, port)) #Binds with the ip on port 5555
+  x = s.recvfrom(1024) #receives message from the client
+  clientip = x[1][0] #Shows ClientIP
+  encodedMessage = x[0] #Shows Message In Bytes[DataType]
+  message = encodedMessage.decode() #Decodes Byte DataType to Str
+  print(clientip + ": " + message)#Displays the message with ClientIP
